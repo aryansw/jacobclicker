72db081b200f9b2d631fb6751d552f2eefce7a3c
A-Wadhwani <wadhwani@purdue.edu>
2023-02-25 17:13:45 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,37 @@
-import { Chip, Avatar, Box } from '@mui/material'
-import type { NextPage } from 'next'
-import Image from "@mui/material"
+import { Chip, Avatar, Box, Stack, Typography, Button } from "@mui/material";
+import type { NextPage } from "next";
+import Image from "@mui/material";
 
-const Home: NextPage = () => <>
-    <Box sx={{
-        'display': 'flex',
-        'background': 'repeating-conic-gradient(#8A351C 0deg 30deg, #B9522E 30deg 60deg);',
-        'width': '100%',
-        'minHeight': '100vh',
-        'alignItems': 'center',
-        'justifyContent': 'center',
-    }}>
-        <img src="/banner.svg" width='60%' />
-    </Box>
-</>
+const Home: NextPage = () => (
+    <>
+        <Box
+            sx={{
+                display: "flex",
+                background:
+                    "repeating-conic-gradient(#8A351C 0deg 30deg, #B9522E 30deg 60deg);",
+                width: "100%",
+                minHeight: "100vh",
+            }}
+        >
+            <Box
+                sx={{
+                    display: "flex",
+                    backgroundImage: "url(/banner.svg)",
+                    backgroundSize: "65%",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    width: "100%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Stack>
+                    <Typography variant="h1" color="white">Welcome to <br /> Jacob Clicker</Typography>
+                    <Button className="secondary">Play</Button>
+                </Stack>
+            </Box>
+        </Box>
+    </>
+);
 
-export default Home
+export default Home;
