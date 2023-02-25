145aa2473be3c618d9bca79394af97c9eb552c34
jsbbvk <jacobzhang132@gmail.com>
2023-02-25 16:34:42 -0500

    added formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
+import { CacheProvider } from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import Head from 'next/head'
+import { SessionProvider } from 'next-auth/react'
+import * as React from 'react'
+
+import theme from '@/styles/theme'
+import createEmotionCache from '@/utils/createEmotionCache'
+
+import type { EmotionCache } from '@emotion/react'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+// eslint-disable-next-line react/function-component-definition
+export default function App(props: AppProps & { emotionCache?: EmotionCache }) {
+  const {
+    Component,
+    emotionCache = clientSideEmotionCache,
+    pageProps: { session, ...pageProps },
+  } = props
+  return (
+    <SessionProvider session={session}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </SessionProvider>
+  )
 }
